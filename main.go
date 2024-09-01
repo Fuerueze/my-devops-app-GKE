9b6a7d72be01e77ac098163e5c0dd89ea526c7f4
Fürüze Saritoprak <fuerueze.saritoprak@student.htw-berlin.de>
2024-09-01 21:56:06 +0300

    logs hinzugefügt

--- main.go
+++ main.go
@@ -2,14 +2,20 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
+	log.Println("Received request: ", r.URL.Path)
 	fmt.Fprintf(w, "Hello, GKE, trigger test!")
 }
 
 func main() {
 	http.HandleFunc("/", handler)
-	http.ListenAndServe(":8080", nil)
+	log.Println("Starting server on port 8080")
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		log.Fatalf("Server failed: %v", err)
+	}
 }
