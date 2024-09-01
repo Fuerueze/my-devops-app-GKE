7ffcb77649e694f96e88b4befccbac7f1bbd5ef5
Fürüze Saritoprak <fuerueze.saritoprak@student.htw-berlin.de>
2024-09-02 02:50:42 +0300

    trigger test

--- main.go
+++ main.go
@@ -8,7 +8,7 @@ import (
 
 func handler(w http.ResponseWriter, r *http.Request) {
 	log.Printf("Received request: %s", r.URL.Path)
-	fmt.Fprintf(w, "Hello, GKE, trigger funktioniert!")
+	fmt.Fprintf(w, "Hello, GKE, trigger funktioniert!!!")
 }
 
 func main() {
