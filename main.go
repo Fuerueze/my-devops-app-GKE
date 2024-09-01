a89f981518e0d8ede1ddb278b4b35c7870f08a2e
Fürüze Saritoprak <fuerueze.saritoprak@student.htw-berlin.de>
2024-09-02 00:31:50 +0300

    localhostproblem gelöst

--- main.go
+++ main.go
@@ -7,15 +7,14 @@ import (
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	log.Println("Received request: ", r.URL.Path)
+	log.Printf("Received request: %s", r.URL.Path)
 	fmt.Fprintf(w, "Hello, GKE, trigger test!")
 }
 
 func main() {
 	http.HandleFunc("/", handler)
 	log.Println("Starting server on port 8080")
-	err := http.ListenAndServe(":8080", nil)
-	if err != nil {
+	if err := http.ListenAndServe(":8080", nil); err != nil {
 		log.Fatalf("Server failed: %v", err)
 	}
 }
