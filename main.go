ca63b3e7c5b066b4841d14214edaeaed05a4cd86
Fürüze Saritoprak <fuerueze.saritoprak@student.htw-berlin.de>
2024-09-08 14:39:28 +0200

    test deployment

--- main.go
+++ main.go
@@ -8,7 +8,7 @@ import (
 
 func handler(w http.ResponseWriter, r *http.Request) {
 	log.Printf("Received request: %s", r.URL.Path)
-	fmt.Fprintf(w, "Hello, GKE, test, test,test!")
+	fmt.Fprintf(w, "Hello, GKE, ich glaube es funktioniert!!!")
 }
 
 func main() {
