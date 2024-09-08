29fd56ffb37a6d9b5f2d1295fcd420985c1bb302
Fürüze Saritoprak <fuerueze.saritoprak@student.htw-berlin.de>
2024-09-08 15:59:28 +0200

    test

--- main.go
+++ main.go
@@ -8,7 +8,7 @@ import (
 
 func handler(w http.ResponseWriter, r *http.Request) {
 	log.Printf("Received request: %s", r.URL.Path)
-	fmt.Fprintf(w, "Hello, GKE, TEST,LOL,LOL")
+	fmt.Fprintf(w, "Hello, GKE, APFELLIEBE")
 }
 
 func main() {
